8c3a715c3c22251e44648ed74061e76166d80fc1
Glorydark <1083215364@qq.com>
2022-07-15 18:09:45 +0800

    Add basic target selector

--- src/main/java/glorydark/sudo/SudoCommand.java
+++ src/main/java/glorydark/sudo/SudoCommand.java
@@ -4,10 +4,18 @@ import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandEnum;
+import cn.nukkit.command.data.CommandParamType;
+import cn.nukkit.command.data.CommandParameter;
+
+import java.util.Collection;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SudoCommand extends Command {
     public SudoCommand(String command) {
         super(command);
+        this.commandParameters.put("default", new CommandParameter[]{CommandParameter.newType("player", CommandParamType.TARGET),CommandParameter.newType("sudo content", CommandParamType.STRING)});
         this.setUsage("/sudo <player> <command/message> | [Example] /sudo steve /me 233");
     }
 
@@ -41,6 +49,64 @@ public class SudoCommand extends Command {
     }
 
     public void parseSudoCommand(CommandSender sender, String player, String sudo){
+        switch (player){
+            case "@a":
+                Server.getInstance().getOnlinePlayers().values().forEach(p -> parseSudoCommand(sender, p.getName(), sudo));
+                return;
+            case "@p":
+                if(!sender.isPlayer()){ sender.sendMessage("[§cSudo§f] You should use '@p' in game!"); return; }
+                AtomicReference<Player> nearest = new AtomicReference<>();
+                AtomicReference<Double> nearestDistance = new AtomicReference<>();
+                AtomicReference<Player> relativeNearest = new AtomicReference<>();
+                AtomicReference<Double> relativeNearestDistance = new AtomicReference<>();
+                Server.getInstance().getOnlinePlayers().values().forEach(player1 -> {
+                    if(!player1.getName().equals(sender.getName())) {
+                        if (player1.getLevel().equals(((Player) sender).getLevel())) {
+                            if (nearest.get() == null) {
+                                nearest.set(player1);
+                                nearestDistance.set(((Player) sender).distance(player1));
+                            } else {
+                                Double dis = ((Player) sender).distance(player1);
+                                if (dis > nearestDistance.get()) {
+                                    nearest.set(player1);
+                                    nearestDistance.set(dis);
+                                }
+                            }
+                        } else {
+                            if (relativeNearest.get() == null) {
+                                relativeNearest.set(player1);
+                            } else {
+                                Double dis = ((Player) sender).distance(player1);
+                                if (dis > relativeNearestDistance.get()) {
+                                    relativeNearest.set(player1);
+                                    relativeNearestDistance.set(dis);
+                                }
+                            }
+                        }
+                    }
+                });
+                if (nearest.get() != null) {
+                    parseSudoCommand(sender, nearest.get().getName(), sudo);
+                    return;
+                }
+                if (relativeNearest.get() != null) {
+                    parseSudoCommand(sender, nearest.get().getName(), sudo);
+                    return;
+                }
+                parseSudoCommand(sender, sender.getName(), sudo);
+                return;
+            case "@r":
+                Collection<Player> collection = Server.getInstance().getOnlinePlayers().values();
+                if(collection.size() > 0) {
+                    Player[] players = collection.toArray(new Player[collection.size() - 1]);
+                    Random random = new Random();
+                    Integer index = random.nextInt(players.length);
+                    parseSudoCommand(sender, players[index].getName(), sudo);
+                }else{
+                    sender.sendMessage("[§cSudo§f] §l§cThere is no player online now!");
+                }
+                return;
+        }
         if(sudo.startsWith("/")){
             Player p = Server.getInstance().getPlayer(player);
             if(p != null && p.isOnline()){
